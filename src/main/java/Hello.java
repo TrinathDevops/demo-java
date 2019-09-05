4e7c271dc073fa083a679ceef7f066ca24e73f3b
kishanbommi <39335053+kishanbommi@users.noreply.github.com>
2019-09-05 16:15:44 +0530

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -10,7 +10,7 @@ public class Hello extends HttpServlet {
 
    public void init() throws ServletException {
       // Do required initialization
-      message = "welcome to Mr.Cooper";
+      message = "Hello World: src/main/java/Hello.java";
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response)
